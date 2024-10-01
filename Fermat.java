7445d0dda661c3bfb0da05887f3d6de0843ea029
Poornima K <164195809+PoorniKR@users.noreply.github.com>
2024-10-01 17:58:19 +0530

    Cryptography

--- Fermat.java
+++ Fermat.java
@@ -1,43 +1,37 @@
 import java.util.*;
 
 public class Fermat {
-    static int gcd(int a, int b)
-    {
+    static int gcd(int a, int b) {
         if (b == 0) {
             return a;
-        }
-        else {
+        } else {
             return gcd(b, a % b);
         }
     }
- 
-    static int power(int x, int y, int m)
-    {
+
+    static int power(int x, int y, int m) {
         if (y == 0)
             return 1;
         int p = power(x, y / 2, m) % m;
         p = (p * p) % m;
- 
+
         return (y % 2 == 0) ? p : (x * p) % m;
     }
- 
 
-    static void modInverse(int a, int m)
-    {
-        if (gcd(a, m) != 1)
-            System.out.print("Inverse doesn't exist");
-        else {
-            System.out.print(
-                "Modular multiplicative inverse is "
-                + power(a, m - 2, m));
-        }
+    static int fermatModularExponentiation(int a, int e, int m) {
+        return power(a, e, m);
     }
- 
-    public static void main(String[] args)
-    {
-        Scanner s =new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter base (a): ");
         int a = s.nextInt();
+        System.out.print("Enter exponent (e): ");
+        int e = s.nextInt();
+        System.out.print("Enter modulus (m): ");
         int m = s.nextInt();
-        modInverse(a, m);
+        
+        int result = fermatModularExponentiation(a, e, m);
+        System.out.println(a + "^" + e + " mod " + m + " = " + result);
     }
 }
